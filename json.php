9c981a2fb91f57917cc45955768dc94147a8b016
Lars Moelleken <lars@moelleken.org>
2014-06-23 02:14:54 +0200

    [+]: update "json.php"

--- json.php
+++ json.php
@@ -6,11 +6,12 @@ require_once('inc_global.php');
 $callback = $purifier->purify($_REQUEST['callback']);
 
 if (count($rssArray) > 0) {
-	header("content-type: application/json;");
 
-	if ($callback) {
-		echo $callback . '(' . json_encode($rssArray) . ')';
-	} else {
-		echo json_encode($rssArray);
-	}
+  if ($callback) {
+    header("content-type: text/javascript");
+    echo $callback . '(' . json_encode($rssArray) . ')';
+  } else {
+    header("content-type: application/json;");
+    echo json_encode($rssArray);
+  }
 }
